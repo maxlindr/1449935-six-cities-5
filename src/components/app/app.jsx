5ad72681d04a19c9f47173f828166c31cd028cd5
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-10-04 22:00:11 +0300

    Добавлены маршруты и компоненты страниц

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from '../main/main';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
 
 const App = (props) => {
   const {rentalOffersNumber} = props;
@@ -11,6 +14,15 @@ const App = (props) => {
       <Switch>
         <Route exact path="/">
           <Main rentalOffersNumber={rentalOffersNumber} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
         </Route>
       </Switch>
     </BrowserRouter>
