c4eff813df36f81fcbcd56b1ab287a1c817fa99a
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-10-13 20:48:55 +0300

    Создан компонент PageFooter

---
+++ src/components/page-footer/page-footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
+const PageFooter = (props) => (
+  <footer className={props.container ? `footer container` : `footer`}>
+    <Link className="footer__logo-link" to="/">
+      <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width={64} height={33} />
+    </Link>
+  </footer>
+);
+
+PageFooter.propTypes = {
+  container: PropTypes.bool
+};
+
+export default PageFooter;
