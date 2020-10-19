140b658445d4f3eeb104a87618c2621ddeb49f7f
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-10-19 19:07:24 +0300

    Создан Enum с городами

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,12 @@ export const OfferCardType = {
   NEAR_PLACE: `near_place`,
   CITIES: `cities`
 };
+
+export const City = {
+  PARIS: `Paris`,
+  COLOGNE: `Cologne`,
+  BRUSSELS: `Brussels`,
+  AMSTERDAM: `Amsterdam`,
+  HAMBURG: `Hamburg`,
+  DUSSELDORF: `Dusseldorf`
+};
