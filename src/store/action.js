e8d81a89b156321fbf7151282c5ac822d37b0964
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-10-24 22:13:40 +0300

    В redux добавлено новое действие: обновление offer

--- src/store/action.js
+++ src/store/action.js
@@ -1,6 +1,7 @@
 export const ActionType = {
   CHANGE_CITY: `CHANGE_CITY`,
   UPDATE_LOCAL_OFFERS: `UPDATE_LOCAL_OFFERS`,
+  UPDATE_OFFER: `UPDATE_OFFER`,
 };
 
 export const ActionCreator = {
@@ -11,5 +12,9 @@ export const ActionCreator = {
   updateLocalOffers: (offers, cityName) => ({
     type: ActionType.UPDATE_LOCAL_OFFERS,
     payload: offers.filter((offer) => offer.location.city.name === cityName)
-  })
+  }),
+  updateOffer: (offer) => ({
+    type: ActionType.UPDATE_OFFER,
+    payload: offer
+  }),
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,6 +6,10 @@ const reducer = (state, action) => {
       return Object.assign({}, state, {currentCity: action.payload});
     case ActionType.UPDATE_LOCAL_OFFERS:
       return Object.assign({}, state, {localOffers: action.payload});
+    case ActionType.UPDATE_OFFER:
+      const newOffer = action.payload;
+      const newOffers = state.offers.map((item) => (item.id === newOffer.id) ? newOffer : item);
+      return Object.assign({}, state, {offers: newOffers});
   }
 
   return state;
