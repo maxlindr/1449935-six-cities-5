ee23b9d940461a7b3ada8fd2c2f1f59bcbaa6172
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2020-11-25 18:35:22 +0300

    Клик по кнопке «Избранное», если пользователь не авторизован, приводит к переходу на страницу Sign In

--- src/store/actions/api-actions/update-favorite-status.js
+++ src/store/actions/api-actions/update-favorite-status.js
@@ -1,16 +1,24 @@
 import {handleErrorWithAlert} from './error-handlers';
 import {OfferAdapter} from '../../../services/api-data-adapters';
 import {ActionCreator} from '../action';
-
+import {AppRoute, HttpCode} from '../../../constants';
 
 export const updateFavoriteStatus = (offerId, status) => (dispatch, _getState, api) => (
   api.post(`/favorite/${offerId}/${Number(status)}`)
     .then(({data}) => {
-      dispatch(ActionCreator.updateOffer(
-          OfferAdapter.toClient(data))
+      dispatch(
+          ActionCreator.updateOffer(
+              OfferAdapter.toClient(data)
+          )
       );
     })
     .catch((err) => {
-      handleErrorWithAlert(dispatch, err);
+      if (err.response.status === HttpCode.UNAUTHORIZED) {
+        dispatch(
+            ActionCreator.goToRoute(AppRoute.LOGIN)
+        );
+      } else {
+        handleErrorWithAlert(dispatch, err);
+      }
     })
 );
